5d6f6067f841877fb5622143986be17e4c19b6c0
IsmailAtaev <issyataew@gmail.com>
2022-01-22 17:28:09 +0300

    Finish lessons 4

--- src/main/java/com/ataew/springcourse/controllers/FirstController.java
+++ src/main/java/com/ataew/springcourse/controllers/FirstController.java
@@ -4,13 +4,20 @@ package com.ataew.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam("surname") String surname) {
+
+        System.out.println("hello, " + name + " " + surname);
+
         return "first/hello";
     }
 
