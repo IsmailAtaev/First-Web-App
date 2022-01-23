6829be729ab65183ff9c50f2e4e0bc0b003490b7
IsmailAtaev <issyataew@gmail.com>
2022-01-23 14:45:29 +0300

    Lessons 5 fixing create new method

--- src/main/java/com/ataew/springcourse/controllers/FirstController.java
+++ src/main/java/com/ataew/springcourse/controllers/FirstController.java
@@ -6,12 +6,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
+
     @GetMapping("/hello")
     public String helloPage(@RequestParam("a") Integer a,
                             @RequestParam("b") Integer b,
@@ -43,6 +43,32 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/cal")
+    public String calculate(@RequestParam("a") Integer a, @RequestParam("b") Integer b,
+                            @RequestParam("action") String action, Model model) {
+        double result = 0.0;
+        System.out.println("A = " + a + "" + "\tB = " + b);
+
+        switch (action) {
+            case "multiplication":
+                result = Double.valueOf(a * b);
+                break;
+            case "addition":
+                result = Double.valueOf(a + b);
+                break;
+            case "subtraction":
+                result = Double.valueOf(a - b);
+                break;
+            case "division":
+                result = Double.valueOf(a / b);
+                break;
+            default:
+                break;
+        }
+        model.addAttribute("message", "Result =  " + String.valueOf(result));
+        return "first/cal";
     }
 
 }
