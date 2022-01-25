03ba673a85b55d52343ed7f1d97c713ec7a68d8c
IsmailAtaev <issyataew@gmail.com>
2022-01-25 09:47:53 +0300

    Lessons 6 create dao person

---
+++ src/main/java/com/ataew/springcourse/dao/PersonDAO.java
@@ -0,0 +1,21 @@
+package com.ataew.springcourse.dao;
+
+import com.ataew.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+
+}
