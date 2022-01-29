0fc1438629a63f633e2de74e2679b664bff78154
IsmailAtaev <issyataew@gmail.com>
2022-01-29 19:46:41 +0300

    Lessons 8

--- src/main/java/com/ataew/springcourse/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/ataew/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,6 +1,11 @@
 package com.ataew.springcourse.config;
 
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.rmi.ServerException;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -15,6 +20,19 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
     }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+    }
+
+
 }
--- src/main/java/com/ataew/springcourse/controllers/PeopleController.java
+++ src/main/java/com/ataew/springcourse/controllers/PeopleController.java
@@ -14,18 +14,17 @@ public class PeopleController {
 
     private final PersonDAO personDAO;
 
+
     @Autowired
     public PeopleController(PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
-
 
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
-
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
@@ -42,6 +41,23 @@ public class PeopleController {
     public String create(@ModelAttribute("person") Person person) {
         personDAO.save(person);
         return "redirect:/people";  // redirect
+    }
 
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
     }
 }
